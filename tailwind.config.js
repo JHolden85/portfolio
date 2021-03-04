575eb648670015db51f5d903f8725dbec66eef4b
Jeremiah Holden <jeremiah@cardconnectcp.com>
2021-03-03 20:14:07 -0800

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      green: colors.green
+    },
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
